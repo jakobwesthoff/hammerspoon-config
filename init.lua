eb236350637fae092c0ebab5b38716e9b4558ac1
Jakob Westhoff <jakob@westhoffswelt.de>
2018-02-28 12:31:13 +0100

    Reformat code

--- init.lua
+++ init.lua
@@ -2,54 +2,54 @@
 -- pushToTalk.init{"fn", "ctrl"}
 
 local keystrokeToApp = require("keystrokeToApp")
-keystrokeToApp.register("Slack", {"cmd"}, "k", true)
+keystrokeToApp.register("Slack", { "cmd" }, "k", true)
 
 
 local screens = {
-  internal = "Color LCD",
-  external = "Thunderbolt Display"
+    internal = "Color LCD",
+    external = "Thunderbolt Display"
 }
 
 local windowLayouts = require("windowLayouts")
 windowLayouts.initialize("6x6")
 windowLayouts.registerLayout("Zoom on External", {
-  {app="zoom.us", grid=hs.geometry.new(0,0,6,6), screen=screens.external},
-  {app="Google Chrome", grid=hs.geometry.new(0,0,6,6), screen=screens.internal},
-  {app="PhpStorm", grid=hs.geometry.new(0,0,6,6), screen=screens.internal},
-  {app="Messages", grid=hs.geometry.new(3,0,6,6), screen=screens.internal},
-  {app="Atom", grid=hs.geometry.new(0,0,6,6), screen=screens.internal},
-  {app="Sequel Pro", grid=hs.geometry.new(0,0,6,6), screen=screens.internal},
-  {app="Tyme 2", grid=hs.geometry.new(0,3,6,6), screen=screens.internal},
-  {app="Fantastical", grid=hs.geometry.new(0,0,6,6), screen=screens.internal},
-  {app="Airmail", grid=hs.geometry.new(0,0,6,6), screen=screens.internal},
-  {app="iTerm2", grid=hs.geometry.new(0,0,6,6), screen=screens.internal},
-  {app="Slack", grid=hs.geometry.new(0,0,6,6), screen=screens.internal},
+    { app = "zoom.us", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.external },
+    { app = "Google Chrome", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.internal },
+    { app = "PhpStorm", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.internal },
+    { app = "Messages", grid = hs.geometry.new(3, 0, 6, 6), screen = screens.internal },
+    { app = "Atom", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.internal },
+    { app = "Sequel Pro", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.internal },
+    { app = "Tyme 2", grid = hs.geometry.new(0, 3, 6, 6), screen = screens.internal },
+    { app = "Fantastical", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.internal },
+    { app = "Airmail", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.internal },
+    { app = "iTerm2", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.internal },
+    { app = "Slack", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.internal },
 })
 
 windowLayouts.registerLayout("Zoom on Internal", {
-  {app="zoom.us", grid=hs.geometry.new(0,0,6,6), screen=screens.internal},
-  {app="Google Chrome", grid=hs.geometry.new(0,0,6,6), screen=screens.external},
-  {app="PhpStorm", grid=hs.geometry.new(0,0,6,6), screen=screens.external},
-  {app="Messages", grid=hs.geometry.new(0,0,3,6), screen=screens.external},
-  {app="Atom", grid=hs.geometry.new(0,0,6,6), screen=screens.external},
-  {app="Sequel Pro", grid=hs.geometry.new(0,0,6,6), screen=screens.external},
-  {app="Tyme 2", grid=hs.geometry.new(3,3,6,6), screen=screens.external},
-  {app="Fantastical", grid=hs.geometry.new(0,0,6,6), screen=screens.external},
-  {app="Airmail", grid=hs.geometry.new(0,0,6,6), screen=screens.external},
-  {app="iTerm2", grid=hs.geometry.new(0,0,3,6), screen=screens.external},
-  {app="Slack", grid=hs.geometry.new(3,0,6,6), screen=screens.external},
+    { app = "zoom.us", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.internal },
+    { app = "Google Chrome", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.external },
+    { app = "PhpStorm", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.external },
+    { app = "Messages", grid = hs.geometry.new(0, 0, 3, 6), screen = screens.external },
+    { app = "Atom", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.external },
+    { app = "Sequel Pro", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.external },
+    { app = "Tyme 2", grid = hs.geometry.new(3, 3, 6, 6), screen = screens.external },
+    { app = "Fantastical", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.external },
+    { app = "Airmail", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.external },
+    { app = "iTerm2", grid = hs.geometry.new(0, 0, 3, 6), screen = screens.external },
+    { app = "Slack", grid = hs.geometry.new(3, 0, 6, 6), screen = screens.external },
 })
 
 windowLayouts.registerLayout("Share Screen Internal", {
-  {app="zoom.us", grid=hs.geometry.new(0,0,3,3), screen=screens.external},
-  {app="Google Chrome", grid=hs.geometry.new(0,0,6,6), screen=screens.external},
-  {app="PhpStorm", grid=hs.geometry.new(0,0,6,6), screen=screens.internal},
-  {app="Messages", grid=hs.geometry.new(0,0,3,6), screen=screens.external},
-  {app="Atom", grid=hs.geometry.new(0,0,6,6), screen=screens.external},
-  {app="Sequel Pro", grid=hs.geometry.new(0,0,6,6), screen=screens.internal},
-  {app="Tyme 2", grid=hs.geometry.new(3,3,6,6), screen=screens.external},
-  {app="Fantastical", grid=hs.geometry.new(0,0,6,6), screen=screens.external},
-  {app="Airmail", grid=hs.geometry.new(0,0,6,6), screen=screens.external},
-  {app="iTerm2", grid=hs.geometry.new(0,0,6,6), screen=screens.internal},
-  {app="Slack", grid=hs.geometry.new(3,0,6,6), screen=screens.external},
+    { app = "zoom.us", grid = hs.geometry.new(0, 0, 3, 3), screen = screens.external },
+    { app = "Google Chrome", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.external },
+    { app = "PhpStorm", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.internal },
+    { app = "Messages", grid = hs.geometry.new(0, 0, 3, 6), screen = screens.external },
+    { app = "Atom", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.external },
+    { app = "Sequel Pro", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.internal },
+    { app = "Tyme 2", grid = hs.geometry.new(3, 3, 6, 6), screen = screens.external },
+    { app = "Fantastical", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.external },
+    { app = "Airmail", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.external },
+    { app = "iTerm2", grid = hs.geometry.new(0, 0, 6, 6), screen = screens.internal },
+    { app = "Slack", grid = hs.geometry.new(3, 0, 6, 6), screen = screens.external },
 })
--- windowLayouts/init.lua
+++ windowLayouts/init.lua
@@ -1,95 +1,96 @@
-local gridConfig='6x6'
-local gridStack={}
-local registeredLayouts={}
-local menubarIcon = nil
+local gridConfig = '6x6'
+local gridStack = {}
+local registeredLayouts = {}
+local menubarIcon
 
 function saveGridConfig(newGridConfig)
-  gridConfig=newGridConfig
+    gridConfig = newGridConfig
 end
 
 function getGridConfig()
-  return gridConfig
+    return gridConfig
 end
 
 function popGrid()
-  local poppedElement = table.remove(gridStack)
-  if poppedElement == nil then
-    return
-  end
-  hs.grid.setGrid(poppedElement, nil, nil)
+    local poppedElement = table.remove(gridStack)
+    if poppedElement == nil then
+        return
+    end
+    hs.grid.setGrid(poppedElement, nil, nil)
 end
 
 function pushGrid(gridGeometry)
-  local currentGrid = hs.grid.getGrid()
-  table.insert(gridStack, currentGrid)
-  hs.grid.setGrid(gridGeometry, nil, nil)
+    local currentGrid = hs.grid.getGrid()
+    table.insert(gridStack, currentGrid)
+    hs.grid.setGrid(gridGeometry, nil, nil)
 end
 
 function moveWindowsByApplication(appName, gridGeometry, screen)
     local app = hs.appfinder.appFromName(appName)
     if app == nil then
-      return
+        return
     end
 
     local windows = app:allWindows()
     if next(windows) == nil then
-      return
+        return
     end
 
     pushGrid(getGridConfig())
     for index, window in ipairs(windows) do
-      local realWindowLocation = hs.grid.getCell(gridGeometry, screen)
-      window:move(realWindowLocation)
+        local realWindowLocation = hs.grid.getCell(gridGeometry, screen)
+        window:move(realWindowLocation)
     end
     popGrid()
 end
 
 function addLayout(name, layout)
-  registeredLayouts[name] = layout
+    registeredLayouts[name] = layout
 end
 
 function activateLayout(name)
-  if registeredLayouts[name] == nil then
-    return
-  end
+    if registeredLayouts[name] == nil then
+        return
+    end
 
-  local layout = registeredLayouts[name]
-  for index, config in ipairs(layout) do
-    moveWindowsByApplication(config["app"], config["grid"], config["screen"])
-  end
+    local layout = registeredLayouts[name]
+    for index, config in ipairs(layout) do
+        moveWindowsByApplication(config["app"], config["grid"], config["screen"])
+    end
 end
 
 function loadMenubarIcon()
-  local iconPath = hs.configdir .. "/windowLayouts/icons"
-  return hs.image.imageFromPath(iconPath .. "/window-restore.pdf"):setSize({w = 16, h = 16})
+    local iconPath = hs.configdir .. "/windowLayouts/icons"
+    return hs.image.imageFromPath(iconPath .. "/window-restore.pdf"):setSize({ w = 16, h = 16 })
 end
 
 function initMenubarIcon()
-  menubarIcon = hs.menubar.new()
-  menubarIcon:setIcon(loadMenubarIcon())
-  menubarIcon:setMenu(function()
-    local menubarEntries = {}
-    for name, layout in pairs(registeredLayouts) do
-      table.insert(menubarEntries, {
-        title = name, fn = function()
-          activateLayout(name)
+    menubarIcon = hs.menubar.new()
+    menubarIcon:setIcon(loadMenubarIcon())
+    menubarIcon:setMenu(function()
+        local menubarEntries = {}
+        for name, layout in pairs(registeredLayouts) do
+            table.insert(menubarEntries, {
+                title = name,
+                fn = function()
+                    activateLayout(name)
+                end
+            })
         end
-      })
-    end
-    return menubarEntries
-  end)
+        return menubarEntries
+    end)
 end
 
 -- Public interface
 local windowLayouts = {}
 
 windowLayouts.initialize = function(gridConfig)
-  saveGridConfig(gridConfig)
-  initMenubarIcon()
+    saveGridConfig(gridConfig)
+    initMenubarIcon()
 end
 
 windowLayouts.registerLayout = function(name, layout)
-  addLayout(name, layout)
+    addLayout(name, layout)
 end
 
 return windowLayouts
